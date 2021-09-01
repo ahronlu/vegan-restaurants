dcf94ca5b65c6119d140bd5a01856f50b0440b81
Aharon Lubitsch <59685123+ahronlu@users.noreply.github.com>
2021-09-01 05:49:43 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -15,24 +15,6 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block",
-    },
-  },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(1),
-      width: "auto",
-    },
   },
 }));
 
@@ -43,14 +25,6 @@ export default function SearchAppBar() {
     <div className={classes.root}>
       <AppBar position="static" style={{ background: "#212121" }}>
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton>
           <Link href="/">
             <Typography
               className={classes.title}
@@ -58,7 +32,7 @@ export default function SearchAppBar() {
               noWrap
               style={{ cursor: "pointer" }}
             >
-              מסעדות טבעוניות
+              VF
             </Typography>
           </Link>
         </Toolbar>
