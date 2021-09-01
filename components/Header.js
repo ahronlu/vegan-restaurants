e4441f50edc66a08d15559e2af2b3377483bb869
Aharon Lubitsch <59685123+ahronlu@users.noreply.github.com>
2021-09-01 05:50:57 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,8 @@
 import Link from "next/link";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import { alpha, makeStyles } from "@material-ui/core/styles";
-import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => ({
   root: {
